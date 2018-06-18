26c53fc43f1452639bd637dc57f4f475289b7ce8
ludoguenet <ludovicguenet@gmx.com>
2018-06-18 12:42:50 +0200

    editing user profile system (to improve later)

---
+++ src/AppBundle/Form/AvatarType.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ludovic
+ * Date: 17/06/18
+ * Time: 21:52
+ */
---
+++ src/AppBundle/Repository/AvatarRepository.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ludovic
+ * Date: 18/06/18
+ * Time: 11:56
+ */
